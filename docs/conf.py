87a352a3b721d234dd894ff4b2d411cc6e68e850
Gerson J. Ferreira <gersonfjunior@yahoo.com.br>
2020-10-17 19:14:33 -0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -28,8 +28,9 @@ author = 'Gerson J. Ferreira'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "recommonmark",
-    "sphinx.ext.mathjax"
+    'recommonmark',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.githubpages'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
